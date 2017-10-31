4d1f66c0ff7936cec6aecd611c0a4299ee25320a
Dawid Więcko <dwiecko@INF.UG.EDU.PL>
2017-10-31 10:52:18 +0100

    Add project files.

---
+++ BudgetApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BudgetApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
